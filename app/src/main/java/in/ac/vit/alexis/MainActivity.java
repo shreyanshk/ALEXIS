e70fcb70451da6a1aef0e735a7ca02ebe74232af
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-04-15 16:05:35 +0530

    add permissions

--- app/src/main/java/in/ac/vit/alexis/MainActivity.java
+++ app/src/main/java/in/ac/vit/alexis/MainActivity.java
@@ -1,13 +1,29 @@
 package in.ac.vit.alexis;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.Manifest;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //checking for the permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_DENIED) {
+            Intent intent = new Intent(this, IntroActivity.class);
+            startActivity(intent);
+        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            Intent intent = new Intent(this, IntroActivity.class);
+            startActivity(intent);
+        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+            Intent intent = new Intent(this, IntroActivity.class);
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
 }
