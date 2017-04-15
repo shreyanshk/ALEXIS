15b91738dc2d3f766ccb52ba63995e1d21b6faf5
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-04-15 14:52:15 +0530

    intro screen base

--- app/src/main/java/in/ac/vit/alexis/IntroActivity.java
+++ app/src/main/java/in/ac/vit/alexis/IntroActivity.java
@@ -1,6 +1,8 @@
 package in.ac.vit.alexis;
 
 import agency.tango.materialintroscreen.*;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.Manifest;
@@ -8,7 +10,7 @@ import android.view.View;
 
 
 /**
- * Created by Rajesh on 15-04-2017.
+ * Created by Shreyansh Khajanchi on 15-04-2017.
  */
 
 public class IntroActivity extends MaterialIntroActivity {
@@ -19,17 +21,21 @@ public class IntroActivity extends MaterialIntroActivity {
         addSlide(new SlideFragmentBuilder()
                         .backgroundColor(R.color.colorPrimary)
                         .buttonsColor(R.color.colorAccent)
-                        .possiblePermissions(new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.READ_SMS})
-                        .neededPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+                        .neededPermissions((new String[]{
+                                        Manifest.permission.ACCESS_FINE_LOCATION,
+                                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                                        Manifest.permission.INTERNET,
+                                }))
                         .image(agency.tango.materialintroscreen.R.drawable.ic_next)
                         .title("Help support the needly.")
-                        .description("Description 3")
+                        .description("Here comes the description.")
                         .build(),
                 new MessageButtonBehaviour(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        showMessage("We provide solutions to make you love your work");
+                        Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                        startActivity(intent);
                     }
-                }, "with love"));
+                }, "Launch!"));
     }
 }
